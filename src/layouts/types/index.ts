5cbcae9dae5f09efbccbe78791fc582beac32986
rightokon3 <rightokon3@gmail.com>
2026-08-01 02:37:53 +0100

    fixed some error

---
+++ src/layouts/types/index.ts
@@ -0,0 +1,50 @@
+export interface NavLink {
+  label: string;
+  href: string;
+}
+
+export interface AppointmentFormData {
+  fullName: string;
+  phoneNumber: string;
+  service: string;
+}
+
+export interface ServiceItem {
+  id: string;
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export interface FeatureItem {
+  id: string;
+  icon: string;
+  title: string;
+}
+
+export interface TestimonialItem {
+  id: string;
+  quote: string;
+  name: string;
+  role?: string;
+}
+
+export interface SpecialistItem {
+  id: string;
+  name: string;
+  title: string;
+  image: string;
+}
+
+export interface NewsItem {
+  id: string;
+  title: string;
+  excerpt: string;
+  image: string;
+  date: string;
+}
+
+export interface FooterLinkGroup {
+  heading: string;
+  links: NavLink[];
+}
